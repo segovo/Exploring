3154be8ecea4d499428939388e42d0f8d73fff20
James <mrsegovo@gmail.com>
2023-06-26 22:34:06 -0500

    More changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,22 +5,24 @@
 	import { onMount, tick } from 'svelte';
 	import { draw } from 'svelte/transition';
 	import * as topojsonClient from 'topojson-client';
-	import { geoPatterson } from 'd3-geo-projection';
+	import { geoPatterson, geoRobinson } from 'd3-geo-projection';
 	import _ from 'lodash';
 	import * as turf from '@turf/turf';
 	// import * as simpleWorldMap from '$lib/map/simpleworld.json';
-	import * as complexWorldMap from '$lib/map/world.json';
+	// import * as complexWorldMap from '$lib/map/world.json';
 	import * as worldMap from '$lib/map/world.json';
+	// import * as veryComplexWorldMap from '$lib/map/ne_10m_admin_0_countries.json';
+
 	// import * as countryJson from '$lib/data/countries.json';
 	import * as countryJson from '$lib/data/mergedData.json';
+	import * as countryCenters from '$lib/data/countrycenters.json';
 	import * as airports from '$lib/data/airports/ne_10m_airports.json';
+	import airportIcon from '$lib/icons/airport.svg';
 
 	const topojson = worldMap;
-	// const geojson = topojsonClient.feature(topojson, Object.keys(topojson.objects)[0]);
 	const geojson = topojsonClient.feature(topojson, Object.keys(topojson.objects)[0]);
-	// const geojson = simpleWorldMap
-	// const topojson = complexWorldMap;
-	// const geojson = topojsonClient.feature(topojson, Object.keys(topojson.objects)[0]);
+	// const geojson = simpleWorldMap;
+
 	const dataset = geojson.features;
 	const countryData = Object.entries(countryJson.default);
 
@@ -34,49 +36,56 @@
 		}
 	});
 
+	let largestArea = 0;
 	dataset.forEach((geojson) => {
+		geojson.properties.area = turf.area(geojson);
+		if (geojson.properties.area > largestArea) {
+			// console.log('! geojson : ', geojson);
+			largestArea = geojson.properties.area;
+		}
+
+		const countryCenter = countryCenters.features.find(
+			(country) => country.properties.ISO === geojson.properties.ISO2
+		);
+		if (countryCenter) {
+			// console.log('Country center: ', countryCenter);
+			geojson.properties.center = countryCenter;
+		} else {
+			geojson.properties.center = turf.centerOfMass(geojson);
+		}
 		// let max_area_polygon;
 		// let max_area = 0;
-		// let centerofmass;
 
-		// console.log('geojson: ', geojson.geometry.coordinates);
 		// geojson.geometry.coordinates.forEach((poly) => {
-		// 	// for (let i = 0; i < poly.length; i++) {
-		// 	// 	if (poly[i].length < 4) {
-		// 	// 		poly.splice(i, 1);
-		// 	// 		return;
-		// 	// 	}
-		// 	// }
-		// 	console.log('pre');
-		// 	const polygon = turf.polygon(poly);
-		// 	console.log('poly set');
+		// 	const depth = getArrayDepth(poly);
+
+		// 	let polygon;
+		// 	if (depth > 2) {
+		// 		polygon = turf.polygon(poly);
+		// 	} else {
+		// 		polygon = turf.multiPolygon(poly);
+		// 	}
+		// 	// const polygon = turf.polygon(poly);
 		// 	const area = turf.area(polygon);
-		// 	console.log('area set');
 		// 	if (area > max_area) {
 		// 		max_area = area;
-		// 		max_area_polygon = polygon; // polygon with the largest area
+		// 		max_area_polygon = polygon;
 		// 	}
-		// 	console.log('loop done');
 		// });
-		// center = turf.centerOfMass(max_area_polygon);
-		// if (max_area_polygon) {
-		// 	centerofmass = turf.centerOfMass(max_area_polygon);
-		// } else {
-		// 	centerofmass = turf.centerOfMass(geojson);
-		// }
 
-		// console.log('max_area_polygon: ', max_area_polygon);
-		geojson.properties.center = turf.centerOfMass(geojson);
+		// if (max_area_polygon) {
+		// 	geojson.properties.center = turf.centerOfMass(max_area_polygon);
+		// } else {
+		// 	geojson.properties.center = turf.centerOfMass(geojson);
+		// 	// console.log('FAILED! ', geojson.properties.name);
+		// }
 	});
 
-	// const geometries = topojson.objects ? Object.values(topojson.objects)[0].geometries : undefined;
-	// $: dataset = geometries ? _.zip(geojson.features, geometries) : [];
-	// $: countryData = Object.values(topoMAP.objects)[0].geometries;
-	// $: console.log(Object.entries(topoCOUNTRIES)[0]);
-	// console.log(topoCOUNTRIES);
-	// $: console.log(dataset[0]);
-	// $: countryData = Object.entries(topoCOUNTRIES);
-	// $: console.log(dataset[0]);
+	function getArrayDepth(obj) {
+		if (Array.isArray(obj)) return 1 + Math.max(...obj.map((t) => getArrayDepth(t)));
+		else return 0;
+	}
+
 	let svg;
 	let d3Svg;
 	let scale = 1;
@@ -84,12 +93,11 @@
 	let mousePos;
 
 	function handleMousemove(e) {
-		console.log('mousemove');
+		// console.log('mousemove: ', projection([e.clientX, e.clientY])[0], projection([e.clientX, e.clientY])[1]);
 		mousePos = { x: e.clientX, y: e.clientY };
 	}
 
 	function zoomed(e) {
-		console.log('zoomed!');
 		const t = e.transform;
 		t.x = Math.min(clientX / 2, Math.max(t.x, clientX / 2 - clientX * t.k));
 		t.y = Math.min(clientY / 2, Math.max(t.y, clientY / 2 - clientY * t.k));
@@ -97,72 +105,91 @@
 		if (e.sourceEvent) mousePos = { x: e.sourceEvent.clientX, y: e.sourceEvent.clientY };
 	}
 
-	let zoom = d3.zoom().scaleExtent([1, 50]).on('zoom', zoomed).clickDistance(10);
-
-	// let dataset = [];
-	// json('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson').then(
-	// 	(data) => {
-	// 		dataset = data.features;
-	// 	}
-	// );
+	let projection = geoPatterson();
 
 	// $: projection = d3
-	// 	.geoNaturalEarth1()
+	// .geoNaturalEarth1()
+	// .scale(clientY / 1 / Math.PI)
+	// .translate([clientX / 2, clientY / 2]);
+	// $: projection = geoPatterson()
 	// 	.scale(clientY / 1 / Math.PI)
 	// 	.translate([clientX / 2, clientY / 2]);
 	// $: path = d3.geoPath(projection);
-	let projection = geoPatterson();
-	// $: path = d3.geoPath().projection({ stream: (stream) => projection.stream(stream) });
-	$: path = getPath(scale);
-	$: bounds = d3
+	// let projection = d3.geoMercator();
+
+	const mapSize = d3
 		.geoPath()
 		.projection({ stream: (stream) => projection.stream(stream) })
 		.bounds(geojson);
+	console.log('mapSize', mapSize);
+	// $: console.log('datad3 ', transform);
+	let zoom = d3
+		.zoom()
+		.scaleExtent([2, 128])
+		.translateExtent(mapSize)
+		.on('zoom', zoomed)
+		.clickDistance(10);
 
-	// $: bounds = d3.geoPath(projection).bounds(dataset);
+	// .translateExtent(mapSize)
+
+	$: path = getPath(scale);
+	// $: path = d3.geoPath(projection);
+
+	$: bounds = d3
+		.geoPath(projection)
+		// .projection({ stream: (stream) => projection.stream(stream) })
+		.bounds(geojson);
+
 	function getPath(s) {
-		return d3
-			.geoPath()
-			.projection({ stream: (stream) => projection.stream(stream) })
-			.pointRadius(0.01 * s);
+		return d3.geoPath(projection);
+		// .projection({ stream: (stream) => projection.stream(stream) })
+		// .pointRadius(0.01 * s);
 	}
+
+	// const initialTransform = { k: 6.147500725152074, x: -2193.8890833797695, y: -634.4553424635553 }
 
 	function centerMap() {
-		// const bounds = d3.geoPath(projection).bounds(dataset);
-
-		scale =
-			0.95 /
-			Math.max((bounds[1][0] - bounds[0][0]) / clientX, (bounds[1][1] - bounds[0][1]) / clientY);
-		projection
-			.scale(scale)
-			.translate([
-				(clientX - scale * (bounds[1][0] + bounds[0][0])) / 2,
-				(clientY - scale * (bounds[1][1] + bounds[0][1])) / 2
-			]);
-		// path = d3.geoPath(projection).pointRadius(0.01 * scale);
-		// path = getPath(scale); // It's important to reset the path, otherwise an height change such as full screen might screw up the map
-
-		if (clientX < 800) d3Svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+		// scale =
+		// 	0.95 /
+		// 	Math.max((bounds[1][0] - bounds[0][0]) / clientX, (bounds[1][1] - bounds[0][1]) / clientY);
+		// projection
+		// 	.scale(scale)
+		// 	.translate([
+		// 		(clientX - scale * (bounds[1][0] + bounds[0][0])) / 2,
+		// 		(clientY - scale * (bounds[1][1] + bounds[0][1])) / 2
+		// 	]);
+		// d3Svg.transition().duration(750).call(zoom.transform, initialTransform);
+		// if (clientX < 800) d3Svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
 	}
+
+	// const initialTransform = { k: 6.147500725152074, x: -2193.8890833797695, y: -634.4553424635553 }
 
 	onMount(async () => {
 		d3Svg = d3.select(svg);
 		d3Svg.call(zoom);
+		d3.zoomIdentity.k = 3;
+		d3.zoomIdentity.x = -720;
+		d3.zoomIdentity.y = -170;
+		transform = d3.zoomIdentity;
+		// setTimeout(() => {
+		// 	console.error('d3: ', d3.zoomIdentity);
+		// 	// const initialTransform = d3.zoomIdentity{ k: 6.147500725152074, x: -2193.8890833797695, y: -634.4553424635553 }
+		// 	// d3.zoomIdentity.k = 6.147500725152074;
+		// 	// d3.zoomIdentity.x =  -2193.8890833797695;
+		// 	// d3.zoomIdentity.y = -634.4553424635553;
+		// 	// transform = d3.zoomIdentity;
+		// 	// d3Svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+		// }, 2000);
 		// centerMap();
 	});
 
 	let clientX;
 	let clientY;
 
-	$: console.log('zoomidentity ', transform);
-	// $: console.log('dataset: ', dataset);
-	// $: console.log('countryData: ', countryData[0]);
 	$: console.log('selected: ', selected);
 	let selected;
 	let hovered;
 
-	// const colors = ['#495AB5', '#2189B6', '#31AA1D', '#AA721D'];
-	// const colors = ['#40814C', '#2A7037', '#1A6428', '#115A1E'];
 	const colors = ['#D7F5A7', '#C3E9A0', '#BCDC89', '#FDF0E6'];
 
 	let activeCountry;
@@ -212,13 +239,18 @@
 	function computeColor(population, color) {
 		if (population) {
 			return colors[color];
-			// return `rgba(150, 255, 150, ${(population / 1392730000) * 8 + 0.4}`;
 		} else {
 			return '#3E3A3A';
 		}
 	}
 
-	$: strokeWidth = transform.k > 4 ? 2 / transform.k : 0;
+	$: strokeWidth = transform.k > 4 ? 4 / transform.k : 0;
+
+	$: console.log(transform.k);
+	// $: console.log(transform.x / transform.k, transform.y / transform.k);
+
+	$: zoomLevel = (transform.k - 1) / 127;
+	// $: console.log('zoomLevel: ', zoomLevel);
 </script>
 
 <svelte:window
@@ -241,6 +273,7 @@
 </div>
 <svg bind:this={svg} width={clientX} height={clientY} viewBox="0 0 {clientX} {clientY}">
 	<g shape-rendering="auto" {transform} fill="white" stroke="black">
+		<circle cx={clientX / 2 - transform.x} cy={clientY / 2 - transform.y} r="10" fill="red" />
 		{#each dataset as data, i}
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -257,18 +290,142 @@
 				class="country"
 			/>
 		{/each}
-        {#each dataset as data, i}
-            <text
-                x={projection(data.properties.center.geometry.coordinates)[0]}
-                y={projection(data.properties.center.geometry.coordinates)[1]}
-                class="country-name">{data.properties.name}</text
-            >
-        {/each}
-		{#if strokeWidth !== 0}
+		{#each dataset as data, i}
+			<!-- font-size={(((data.properties.area / largestArea) * 5) + 2) * (4 / transform.k)} -->
+			<!-- font-size="{Math.max(((data.properties.area / largestArea) + (4 / transform.k))/2, 0.1)}px" -->
+			<!-- size = 0.01   0.23  0.45           -->
+			<!-- {console.log('data.properties.area: ', data.properties.area / largestArea)} -->
+			{#if 10 / transform.k < 4 || data.properties.area / largestArea > 0.06}
+				<text
+					x={projection(data.properties.center.geometry.coordinates)[0]}
+					y={projection(data.properties.center.geometry.coordinates)[1]}
+					font-size={10 / transform.k}
+					class="country-name">{data.properties.name}</text
+				>
+			{/if}
+		{/each}
+		{#if strokeWidth != 0}
 			{#each airports.features as airport}
-				<path d={path(airport)} fill="none" stroke="white" stroke-width={strokeWidth} />
+				{#if zoomLevel > 1}
+					<svg
+						x={projection(airport.geometry.coordinates)[0] - 1 / 2}
+						y={projection(airport.geometry.coordinates)[1] - 1 / 2}
+						width="1px"
+						height="1px"
+						viewBox="0 0 13 13"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<rect
+							x="2.17847"
+							y="0.646447"
+							width="14.5"
+							height="2.16667"
+							transform="rotate(45 2.17847 0.646447)"
+							fill="#584D75"
+							stroke="#FBF0E7"
+							stroke-width="0.5"
+						/>
+						<rect
+							x="2.17847"
+							y="1.47144"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 2.17847 1.47144)"
+							fill="white"
+							stroke="none"
+						/>
+						<rect
+							x="1.85449"
+							y="1.79553"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 1.85449 1.79553)"
+							fill="white"
+							stroke="none"
+						/>
+						<rect
+							x="1.55981"
+							y="2.09009"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 1.55981 2.09009)"
+							fill="white"
+							stroke="none"
+						/>
+						<rect
+							x="10.929"
+							y="10.2218"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 10.929 10.2218)"
+							fill="white"
+							stroke="none"
+						/>
+						<rect
+							x="10.6047"
+							y="10.5459"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 10.6047 10.5459)"
+							fill="white"
+							stroke="none"
+						/>
+						<rect
+							x="10.3103"
+							y="10.8406"
+							width="1"
+							height="0.208333"
+							rx="0.104167"
+							transform="rotate(45 10.3103 10.8406)"
+							fill="white"
+							stroke="none"
+						/>
+						<path
+							d="M3.20972 2.62048L3.3509 3.32641C3.35382 3.34099 3.34096 3.35384 3.32639 3.35092L2.62046 3.20974"
+							stroke="#ECBB5C"
+							stroke-width="0.208333"
+							stroke-linecap="round"
+						/>
+						<path
+							d="M3.94629 3.35706L4.08747 4.06298C4.09039 4.07756 4.07754 4.09041 4.06296 4.0875L3.35703 3.94631"
+							stroke="#ECBB5C"
+							stroke-width="0.208333"
+							stroke-linecap="round"
+						/>
+					</svg>
+				{:else}
+					<!-- <path d={path(airport)} fill="none" stroke="black" stroke-width={strokeWidth} /> -->
+				{/if}
 			{/each}
 		{/if}
+
+		{#if true}
+			{#each airports.features as airport}
+				<!-- 6.835 -->
+				<svg
+					x={projection(airport.geometry.coordinates)[0] - ((18 / transform.k)) / 2}
+					y={projection(airport.geometry.coordinates)[1] - ((18 / transform.k)) / 2}
+					width={(18 / transform.k)}
+					height={(18 / transform.k)}
+					viewBox="0 0 30 30"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M21.5897 19.0897L17.9979 15.4979C17.5105 15.0105 17.6508 14.1876 18.2723 13.8893L28.3662 9.0442C28.9462 8.76584 29.1162 8.02028 28.7144 7.51798L27.8775 6.47193C27.6454 6.18175 27.2715 6.04423 26.9067 6.11484L12.5014 8.90296C12.1865 8.96391 11.8614 8.87009 11.6274 8.65071L4.14575 1.63664C4.04953 1.54644 3.93642 1.47616 3.81293 1.42985L3.45511 1.29566C1.96837 0.738138 0.456035 2.05216 0.800485 3.60218L0.940607 4.23273C0.979487 4.40769 1.06467 4.56899 1.18726 4.69974L8.14803 12.1246C8.36891 12.3602 8.46241 12.6879 8.39908 13.0046L5.60496 26.9752C5.53939 27.3031 5.64201 27.642 5.87843 27.8784L7.01894 29.0189C7.50158 29.5016 8.31523 29.3695 8.62048 28.759L13.4127 19.1746C13.7081 18.5837 14.4851 18.4366 14.9761 18.8785L18.5421 22.0879C18.8229 22.3406 18.9373 22.7299 18.8379 23.0943L17.6527 27.4401C17.5583 27.7863 17.6566 28.1566 17.9103 28.4103L18.6048 29.1048C19.0615 29.5615 19.825 29.4719 20.1635 28.9218L23.3506 23.7428C23.4482 23.5841 23.5883 23.4558 23.755 23.3725L28.3936 21.0532C28.963 20.7685 29.125 20.0312 28.7273 19.5341L27.9103 18.5129C27.6612 18.2015 27.251 18.0679 26.8663 18.1728L22.5599 19.3473C22.2137 19.4417 21.8434 19.3434 21.5897 19.0897Z"
+						fill="#7A4E6A"
+						stroke="white"
+					/>
+				</svg>
+			{/each}
+		{/if}
+
 		{#if selected}
 			<!-- 549460 -->
 
@@ -328,7 +485,7 @@
 	} */
 
 	.country-name {
-		font-size: 3px;
+		/* font-size: 2px; */
 		font-weight: 700;
 		fill: #814b6c;
 		paint-order: stroke;
